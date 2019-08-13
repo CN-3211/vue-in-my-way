7a5c72e7a3b6a2be3317fe6584f0a37d20a0f8d8
黄子涵 <372083746@qq.com>
2019-08-13 09:31:35 +0800

    ref传值和emit传值

---
+++ src/components/refTrans/childRef.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="childRef">
+    {{mydata}}
+    <el-button @click="handleClick" type="primary">主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mydata: 0
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$emit('emitFuc', this.mydata)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
 import TodoList from '@/components/todoList'
-import FormSubmit from '@/components/FormSubmit'
+import FormSubmit from '@/components/formSubmit'
+import RefTrans from '@/components/refTrans/firstRef'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/formSubmit',
       name: 'FormSubmit',
       component: FormSubmit
+    },
+    {
+      path: '/refTrans',
+      name: 'RefTrans',
+      component: RefTrans
     }
   ]
 })
